0c848d3eb90ecb5fb59b404ba7529079be805edc
观 南 <114809528+weqeweqewe@users.noreply.github.com>
2023-03-14 20:12:53 +0800

    Add files via upload

---
+++ disc3.py
@@ -0,0 +1,30 @@
+#1
+def multiply(m, n):
+    if n == 1:
+        return m
+    else:
+        return m + multiply(m, n - 1)
+    
+#2
+def hailstone(n):
+    print(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return 1 + hailstone(n // 2)
+    else:
+        return 1 + hailstone(3 * n + 1)
+    
+
+#3
+def merge(n1, n2):
+    if n1 == 0:
+        return n2
+    elif n2 == 0:
+        return n1
+    elif n1 % 10 < n2 % 10:
+        return merge(n1 // 10, n2) * 10 + n1 % 10
+    else:
+        return merge(n1, n2 // 10) * 10 + n2 % 10
+    
+    
